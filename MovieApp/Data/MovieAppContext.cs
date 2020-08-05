6250bfbb0ff7bf7a1b8526ac0c8fa7187e04d8ff
unknown <alex_s253@hotmail.com>
2020-08-05 23:22:53 +0930

    created scaffolded views and models

---
+++ MovieApp/Data/MovieAppContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieApp.Models;
+
+namespace MovieApp.Data
+{
+    public class MovieAppContext : DbContext
+    {
+        public MovieAppContext(DbContextOptions<MovieAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
